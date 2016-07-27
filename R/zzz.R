2631a8d4208566b9153e05afff61e13560516877
Denis Haine <denis.haine@gmail.com>
2016-07-27 16:20:53 -0400

    Fix namespace issue

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,4 @@
-.onLoad <- function(libname, pkgname) { Rcpp::loadModule("stan_fit4logistic_mod", TRUE) }
+.onLoad <- function(libname, pkgname) {
+  modules <- paste0("stan_fit4", names(stanmodels), "_mod")
+  for (m in modules) loadModule(m, what = TRUE)
+}
